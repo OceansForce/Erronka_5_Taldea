0e721e5821ba0d537e72d2cc623de6e8f490da57
Manex <maranzadieg23wg@ikzubirimanteo.com>
2024-03-14 11:40:23 +0100

    Mugarria 1 egiten

---
+++ Mugarriak/Datu-Baseak/Oracle_Erronka/generated/2024-03-14_08-00-52/.wrapper.sql
@@ -0,0 +1,8 @@
+-- Password for 'erleak' user
+define erleak_password='"DzmCRFcF$WDWyc3q"'
+
+-- Password for 'Emulation' user
+define Emulation_password='"2gJn$epsfl#Dw69e"'
+
+@@"D:\Erronka_5_taldea\Erronka_Taldea_5\Erronka_5_Taldea\Mugarriak\Datu-Baseak\Oracle_Erronka\generated\2024-03-14_08-00-52\master.sql"
+
