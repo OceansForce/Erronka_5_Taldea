de679e428cc2ff2175c6e97fee2c176b7c197265
iarrizabaloa <iarrizabaloai23wg@ikzubirimanteo.com>
2024-04-08 12:11:34 +0200

    webgunea garatzen

--- webgunea/js/ezti.js
+++ webgunea/js/ezti.js
@@ -2,3 +2,8 @@ function load_page(page){
     window.location.href = page;
 }
 
+async function print_eztiak(){
+    let eztiak = await(await fetch("json/eztia.json")).json();
+
+    let taula = "<tr><thead><td>Izena</td><td>Deskripzioa</td><td>Prezioa</td></thead></tr>";
+}
