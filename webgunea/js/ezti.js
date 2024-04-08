d3fab07efbd80341b875d1230d0d768d28b1d3b0
iarrizabaloa <iarrizabaloai23wg@ikzubirimanteo.com>
2024-04-08 12:47:39 +0200

    json eta js bidez produktuak bistarazi

--- webgunea/js/ezti.js
+++ webgunea/js/ezti.js
@@ -6,4 +6,12 @@ async function print_eztiak(){
     let eztiak = await(await fetch("json/eztia.json")).json();
 
     let taula = "<tr><thead><td>Izena</td><td>Deskripzioa</td><td>Prezioa</td></thead></tr>";
+
+    for(i=0; i<eztiak.length; i++){
+
+        let produktuak = "<tr><td>"+eztiak[i].izena+"</td></tr>";
+        taula = taula + produktuak;
+    }
+
+    document.querySelector("#products").innerHTML = taula;
 }
