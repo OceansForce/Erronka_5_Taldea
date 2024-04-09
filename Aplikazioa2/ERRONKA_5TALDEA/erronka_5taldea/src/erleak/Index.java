9d1898051477f487e4ce254974d97229f27ccec8
Manex <maranzadieg23wg@ikzubirimanteo.com>
2024-04-09 13:59:25 +0200

    Programa egiten

--- Aplikazioa2/ERRONKA_5TALDEA/erronka_5taldea/src/erleak/Index.java
+++ Aplikazioa2/ERRONKA_5TALDEA/erronka_5taldea/src/erleak/Index.java
@@ -1,72 +1,119 @@
 package erleak;
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class Index extends JFrame implements ActionListener {
-    private JPanel panel;
+    private JPanel panel1, panel2;
     private JMenu menu1, menu2;
     private JMenuItem mi1, mj1, mj2;
-    private JButton loginButton;
+    private JButton loginButton, erregistratzeko_Button;
     private JTextField bilatzaile;
-    public Index(){
-        super("Erleak");
-        panel = new JPanel(new FlowLayout(FlowLayout.LEFT)); // Usamos un FlowLayout para colocar los elementos en línea
-        setContentPane(panel);
+    private JFrame f_Index = new JFrame();
+
+    public static void main(String[] args){
+
+        new Index().sortu();
+    }
+    public void sortu(){
+        center();
+        north();
+        orria();
+    }
+    public void orria(){
+
+        f_Index.setTitle("Erleak");
+        f_Index.setSize(900, 450);
+        f_Index.setVisible(true);
+        f_Index.setLocationRelativeTo(null);
+        f_Index.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f_Index.setLayout(new BorderLayout());
+    }
+
+    public void north(){
+        panel1 = new JPanel();
+        panel1.setLayout(new BoxLayout(panel1,BoxLayout.X_AXIS));
+
 
         // Lehengo menua
-        JMenuBar mb = new JMenuBar(); // Creamos un JMenuBar para los elementos del menú
+        JMenuBar mb = new JMenuBar();
         menu1 = new JMenu("Zerbitzuak");
         mb.add(menu1);
-        mi1 = new JMenuItem("Kolmenen instalazioak");
-        mi1.addActionListener(this);
+
+        mi1 = new JMenuItem("Erlauntzen instalazioak");
         menu1.add(mi1);
+        mi1.addActionListener(this);
 
         //bigarren menua
         menu2 = new JMenu("Produktuak");
         mb.add(menu2);
+
         mj1 = new JMenuItem("Ezti naturala");
-        mj1.addActionListener(this);
-        mj2 = new JMenuItem("Erleen kolmenak");
-        mj2.addActionListener(this);
+        mj2 = new JMenuItem("Erlauntzak");
         menu2.add(mj1);
         menu2.add(mj2);
+        mj1.addActionListener(this);
+        mj2.addActionListener(this);
 
-        // Añadimos el JMenuBar al panel
-        panel.add(mb);
+        mb.setBorder(new EmptyBorder(0,0,0,500));
+        panel1.add(mb);
+
+        //bilatzailea gehitzeko
+        /*bilatzaile = new JTextField();
+        panel1.add(bilatzaile);
+
+        ImageIcon lupaIrudia = new ImageIcon(".\\lupa.png");
+        ImageIcon lupa_TamainaAldatuta= new ImageIcon(lupaIrudia.getImage().getScaledInstance(25, 20, java.awt.Image.SCALE_SMOOTH));
+        JButton lupa = new JButton(lupa_TamainaAldatuta);
+        panel1.add(lupa);
+        lupa.addActionListener(this);*/
+
 
         //login botoia
-        loginButton = new JButton("Login");
+        ImageIcon pertsonaIrudia = new ImageIcon("./pertsona_icon.png");
+        ImageIcon pertsona_TamainaAldatuta= new ImageIcon(pertsonaIrudia.getImage().getScaledInstance(20, 20, java.awt.Image.SCALE_SMOOTH));
+        loginButton = new JButton("Login", pertsona_TamainaAldatuta);
         loginButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                //login lehioa ireki
-                Login loginWindow = new Login();
-                loginWindow.setVisible(true);
+                new Login().sortu_login();
+                f_Index.dispose();
             }
         });
         //mb.add(Box.createHorizontalGlue()); //botoia eskuinaldean jarriko du
-        panel.add(loginButton);
+        panel1.add(loginButton);
 
-        //bilatzailea gehitzeko
-        bilatzaile = new JTextField(5);
-        bilatzaile.addActionListener(this);
-        panel.add(bilatzaile);
+        ImageIcon pertsonaIrudia2 = new ImageIcon("./Erregistratu.png");
+        ImageIcon pertsona2_TamainaAldatuta= new ImageIcon(pertsonaIrudia2.getImage().getScaledInstance(20, 20, java.awt.Image.SCALE_SMOOTH));
+        erregistratzeko_Button = new JButton("Erregistratu", pertsona2_TamainaAldatuta);
+
+        panel1.add(erregistratzeko_Button);
+
+        f_Index.add(panel1, BorderLayout.NORTH);
 
     }
 
+    public void center(){
+        JPanel panel2 = new JPanel(new FlowLayout(FlowLayout.CENTER));
+        ImageIcon portada_irudia= new ImageIcon(".\\abejas.jpg");
+        JLabel portada= new JLabel(portada_irudia);
+        panel2.add(portada);
+        f_Index.add(panel2, BorderLayout.CENTER);
+    }
     @Override
     public void actionPerformed(ActionEvent e) {
+        if(e.getSource()==mi1){
 
+        } else if (e.getSource()==mj1) {
+
+        } else if (e.getSource()==mj2) {
+
+        }
     }
 
-    public static void main(String[] args){
-        Index index1 = new Index();
-        index1.setBounds(10, 20, 300, 200);
-        index1.setVisible(true);
-        index1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    }
+
 
 
 }
--- Aplikazioa2/ERRONKA_5TALDEA/erronka_5taldea/src/erleak/Login.java
+++ Aplikazioa2/ERRONKA_5TALDEA/erronka_5taldea/src/erleak/Login.java
@@ -1,19 +1,47 @@
 package erleak;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.IOException;
 
 
 public class Login extends JFrame {
+    JFrame f_login= new JFrame();
+    JPanel panel1;
+    JPanelConFondo panel2;
+    public void sortu_login(){
+        nothr();
+        gehitu();
+        login_orria();
+    }
+    public void login_orria(){
 
-    public Login(){
-        super("Login");
-        setSize(250, 200);
+        f_login.setLayout(new FlowLayout());
+        f_login.setTitle("Login");
+        f_login.setVisible(true);
+        f_login.setLocationRelativeTo(null);
+        f_login.setSize(250, 200);
+    }
+
+    public void nothr(){
+        panel2 = new JPanelConFondo(".\\kuadroa.jpg");
+        @Override
+        protected void paintComponent(Graphics g) {
+            super.paintComponent(g);
+            g.drawImage(imagenFondo, 0, 0, null);
+        }
+    }
+
+
+    public void gehitu(){
+
+
         Container content = getContentPane();
 
-        JPanel panel = new JPanel(new GridLayout(4, 2)); //loginarentzat bi lerro eta 3 kolumna kalkulatzen dira elementu denentzat
+        panel1 = new JPanel(new GridLayout(4, 2)); //loginarentzat bi lerro eta 3 kolumna kalkulatzen dira elementu denentzat
         JLabel izena = new JLabel("Izena:");
         JTextField txertatuizen = new JTextField();
 
@@ -26,24 +54,25 @@ public class Login extends JFrame {
         JButton itzuli = new JButton("Atzera");
 
 
-        panel.add(izena);
-        panel.add(txertatuizen);
-        panel.add(pasahitza);
-        panel.add(txertatupass);
+        panel1.add(izena);
+        panel1.add(txertatuizen);
+        panel1.add(pasahitza);
+        panel1.add(txertatupass);
 
         //ondorengoa betegarria gehitzeko da, hau gehituko ez bagenu
         //login botoia adibidez ezkerraldera desplazatuko zen
-        panel.add(new JLabel());
-        panel.add(login);
-        panel.add(erregistratu);
-        panel.add(itzuli);
 
-        getContentPane().add(panel);
+        panel1.add(login);
+        panel1.add(erregistratu);
+        panel1.add(itzuli);
+
+        f_login.add(panel1, BorderLayout.CENTER);
 
         itzuli.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                dispose();
+                new Index().sortu();
+                f_login.dispose();
             }
         });
 
@@ -54,12 +83,33 @@ public class Login extends JFrame {
             }
         });
 
-        setVisible(true);
     }
 
     public static void main(String[] args){
         new Login();
     }
+}
+class JPanelConFondo extends JPanel {
+    private Image imagenFondo;
 
+    public JPanelConFondo(String rutaImagen) {
+        try {
+            imagenFondo = ImageIO.read(getClass().getResource(rutaImagen));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        if (imagenFondo != null) {
+            g.drawImage(imagenFondo, 0, 0, getWidth(), getHeight(), this);
+        }
+    }
+
+    @Override
+    public Dimension getPreferredSize() {
+        return imagenFondo == null ? super.getPreferredSize() : new Dimension(imagenFondo.getWidth(this), imagenFondo.getHeight(this));
+    }
 }
