773e13daa13e95eda2d5aa725624200ca9aa1da7
iarrizabaloa <iarrizabaloai23wg@ikzubirimanteo.com>
2024-04-10 13:14:51 +0200

    login

--- webgunea/js/ezti.js
+++ webgunea/js/ezti.js
@@ -3,15 +3,17 @@ function load_page(page){
 }
 
 async function print_eztiak(){
-    let eztiak = await(await fetch("json/eztia.json")).json();
+    let eztiak = await(await fetch("../json/eztia.json")).json();
 
-    let taula = "<tr><thead><td>Izena</td><td>Deskripzioa</td><td>Prezioa</td></thead></tr>";
+    let taula = "<tr><thead><td>Izena</td><td>Deskripzioa</td><td>Prezioa</td><td>Pisua</td><td>Irudia</td></thead></tr>";
 
     for(i=0; i<eztiak.length; i++){
 
-        let produktuak = "<tr><td>"+eztiak[i].izena+"</td><td>"+eztiak[i].irudia+"</td></tr>";
+        let produktuak = "<tr><td>"+eztiak[i].izena+"</td><td>"+eztiak[i].deskripzioa+"</td><td>"+eztiak[i].prezioa+"</td><td><img src="+eztiak[i].irudi+"></td></tr>";
         taula = taula + produktuak;
     }
 
     document.querySelector("#products").innerHTML = taula;
+
+    localStorage.setItem("eztiak", JSON.stringify(eztiak));
 }
