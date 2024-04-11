389a41b6c855254f1b9ef06fd45505afe169d6fe
iarrizabaloa <iarrizabaloai23wg@ikzubirimanteo.com>
2024-04-11 11:28:30 +0200

    web

--- webgunea/js/ezti.js
+++ webgunea/js/ezti.js
@@ -2,18 +2,42 @@ function load_page(page){
     window.location.href = page;
 }
 
+
+async function hasiSaio(){
+    let erabiltzailea = document.querySelector("#erabiltzaile").value;
+    let pasahitza = document.querySelector("#pasahitz").value;
+
+    let response = await fetch ('json/accounts.json');
+    let kontuak = await response.json();
+    console.log(kontuak);
+    for (i=0; i<kontuak.length; i++){
+
+        if(kontuak[i].erabiltzailea == erabiltzailea && kontuak[i].pasahitza == pasahitza){
+            load_page('erleak.html')
+        }
+    }
+}
+
+function check_enter(event){
+    if(event.keyCode == 13){
+        hasiSaio();
+    }
+}
+
+
 async function print_eztiak(){
-    let eztiak = await(await fetch("../json/eztia.json")).json();
+
+    let eztiak = await(await fetch("json/eztia.json")).json();
 
     let taula = "<tr><thead><td>Izena</td><td>Deskripzioa</td><td>Prezioa</td><td>Pisua</td><td>Irudia</td></thead></tr>";
 
-    for(i=0; i<eztiak.length; i++){
+    let irudia = document.getElementById("irudia").value;
 
-        let produktuak = "<tr><td>"+eztiak[i].izena+"</td><td>"+eztiak[i].deskripzioa+"</td><td>"+eztiak[i].prezioa+"</td><td><img src="+eztiak[i].irudi+"></td></tr>";
+    for(i=0; i<eztiak.length; i++){
+        let produktuak = "<tr><td>"+eztiak[i].izena+"</td><td>"+eztiak[i].deskripzioa+"</td><td>"+eztiak[i].prezioa+"</td><td><img src='media/products/"+eztiak[i].irudia+"'></td></tr>";
         taula = taula + produktuak;
     }
 
     document.querySelector("#products").innerHTML = taula;
-
     localStorage.setItem("eztiak", JSON.stringify(eztiak));
 }
