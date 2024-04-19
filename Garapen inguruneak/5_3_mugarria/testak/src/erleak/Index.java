a460c1bd821805b4845cbe7ef45b8bcae401e08d
iarrizabaloa <iarrizabaloai23wg@ikzubirimanteo.com>
2024-04-19 09:39:21 +0200

    GI mugarriarentzako karpeta

---
+++ Garapen inguruneak/5_3_mugarria/testak/src/erleak/Index.java
@@ -0,0 +1,226 @@
+package erleak;
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Index extends JFrame implements ActionListener {
+    private JPanel panel1, panel2, panel3, panel4, panel5;
+    private JPanel panel_2_3_4_5;
+    private JMenu menu2;
+    private JMenuItem mj1, mj2, mj3;
+    private JButton loginButton, erregistratzeko_Button;
+    private CardLayout card1;
+    private JFrame f_Index = new JFrame();
+
+    public static void main(String[] args){
+
+        new Index().sortu();
+    }
+
+    public void konexioa(){
+        try {
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+           Connection con = DriverManager.getConnection("jdbc:oracle:thin:@10.14.4.124:3306:ORCLCDB", "T5_2", "123");
+        }catch (SQLException | ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    public void sortu(){
+        center();
+        north();
+        orria();
+    }
+    public void orria(){
+
+        f_Index.setTitle("Erleak");
+        f_Index.setSize(900, 450);
+        f_Index.setVisible(true);
+        f_Index.setLocationRelativeTo(null);
+        f_Index.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f_Index.setLayout(new BorderLayout());
+    }
+
+    public void north(){
+        panel1 = new JPanel();
+        panel1.setLayout(new BoxLayout(panel1,BoxLayout.X_AXIS));
+
+
+        //Logoa
+        ImageIcon logoa= new ImageIcon(".\\Irudiak\\logo.png");
+        ImageIcon logoa_aldatuta= new ImageIcon(logoa.getImage().getScaledInstance(50, 35, java.awt.Image.SCALE_SMOOTH));
+        JLabel logo_Irudia= new JLabel(logoa_aldatuta);
+        logo_Irudia.addMouseListener(new MouseListener() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                card1.show(panel_2_3_4_5, "panel2");
+            }
+
+            @Override
+            public void mousePressed(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+
+            }
+        });
+
+        panel1.add(logo_Irudia);
+        // Lehengo menua
+        JMenuBar mb = new JMenuBar();
+
+        //bigarren menua
+        menu2 = new JMenu("Produktuak");
+        mb.add(menu2);
+
+        mj1 = new JMenuItem("Eztia");
+        mj1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                card1.show(panel_2_3_4_5, "panel3");
+            }
+        });
+        mj2 = new JMenuItem("Beste batzuk");
+        mj2.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                card1.show(panel_2_3_4_5, "panel4");
+            }
+        });
+
+        mj3= new JMenuItem("Materiala");
+        mj3.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                card1.show(panel_2_3_4_5, "panel5");
+            }
+        });
+        menu2.add(mj1);
+        menu2.add(mj2);
+        menu2.add(mj3);
+        mj1.addActionListener(this);
+        mj2.addActionListener(this);
+
+        mb.setBorder(new EmptyBorder(0,0,0,530));
+        panel1.add(mb);
+
+        //login botoia
+        ImageIcon pertsonaIrudia = new ImageIcon(".\\Irudiak\\pertsona_icon.png");
+        ImageIcon pertsona_TamainaAldatuta= new ImageIcon(pertsonaIrudia.getImage().getScaledInstance(20, 20, java.awt.Image.SCALE_SMOOTH));
+        loginButton = new JButton("Login", pertsona_TamainaAldatuta);
+        loginButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                new Login().sortu_login();
+                f_Index.dispose();
+            }
+        });
+        //mb.add(Box.createHorizontalGlue()); //botoia eskuinaldean jarriko du
+        panel1.add(loginButton);
+
+        ImageIcon pertsonaIrudia2 = new ImageIcon(".\\Irudiak\\Erregistratu.png");
+        ImageIcon pertsona2_TamainaAldatuta= new ImageIcon(pertsonaIrudia2.getImage().getScaledInstance(20, 20, java.awt.Image.SCALE_SMOOTH));
+        erregistratzeko_Button = new JButton("Erregistratu", pertsona2_TamainaAldatuta);
+        erregistratzeko_Button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                new Erregistratu().sortu_Erregistratu();
+                f_Index.dispose();
+            }
+        });
+
+        panel1.add(erregistratzeko_Button);
+
+        f_Index.add(panel1, BorderLayout.NORTH);
+
+    }
+
+    public void center(){
+        card1= new CardLayout();
+        panel_2_3_4_5= new JPanel();
+        panel_2_3_4_5.setLayout(card1);
+
+        panel2 = new JPanel(new FlowLayout(FlowLayout.CENTER));
+        ImageIcon portada_irudia= new ImageIcon(".\\Irudiak\\abejas.jpg");
+        JLabel portada= new JLabel(portada_irudia);
+        panel2.add(portada);
+
+        panel3 = new JPanel(new BorderLayout());
+
+        ImageIcon eztia_irudia = new ImageIcon(".\\Irudiak\\kuadroa_eztia.jpg");
+        ImageIcon eztia_irudia_aldatuta= new ImageIcon(eztia_irudia.getImage().getScaledInstance(900, 100, java.awt.Image.SCALE_SMOOTH));
+        JLabel eztia_Lirudi= new JLabel(eztia_irudia_aldatuta);
+        panel3.add(eztia_Lirudi, BorderLayout.NORTH);
+
+        String[][] ezti_datuak= {{"Julen","32","5435","Pertsona da."}};
+        String[] kolumna_Izenak_eztia= {"Izena", "Prezioa","Kantitatea", "Deskribapena"};
+        final JTable ezti_tabla= new JTable(ezti_datuak,kolumna_Izenak_eztia);
+        ezti_tabla.setPreferredScrollableViewportSize(new Dimension(700, 225));
+        JScrollPane ezti_sp= new JScrollPane(ezti_tabla);
+
+        panel3.add(ezti_sp, BorderLayout.CENTER);
+
+        panel4= new JPanel(new BorderLayout());
+
+        ImageIcon besteak_irudia = new ImageIcon(".\\Irudiak\\kuadroa_erdia.jpg");
+        ImageIcon besteak_irudia_aldatuta= new ImageIcon(besteak_irudia.getImage().getScaledInstance(900, 100, java.awt.Image.SCALE_SMOOTH));
+        JLabel besteak_Lirudi= new JLabel(besteak_irudia_aldatuta);
+        panel4.add(besteak_Lirudi, BorderLayout.NORTH);
+
+        String[][] besteak_datuak= {{"Julen","32","5435","Pertsona da."}};
+        String[] kolumna_Izenak_besteak= {"Izena", "Prezioa","Kantitatea", "Deskribapena"};
+        final JTable besteak_tabla= new JTable(besteak_datuak,kolumna_Izenak_besteak);
+        besteak_tabla.setPreferredScrollableViewportSize(new Dimension(700, 225));
+        JScrollPane bestak_sp= new JScrollPane(besteak_tabla);
+
+        panel4.add(bestak_sp);
+
+        panel5= new JPanel(new BorderLayout());
+
+
+        panel5.add(besteak_Lirudi, BorderLayout.NORTH);
+
+        String[][] material_datuak= {{"Julen","32","5435","Pertsona da."}};
+        String[] kolumna_Izenak_material= {"Izena", "Prezioa","Kantitatea", "Deskribapena"};
+        final JTable material_tabla= new JTable(material_datuak,kolumna_Izenak_material);
+        material_tabla.setPreferredScrollableViewportSize(new Dimension(700, 225));
+        JScrollPane material_sp= new JScrollPane(material_tabla);
+
+        panel5.add(material_sp);
+
+
+        panel_2_3_4_5.add(panel2,"panel2");
+        panel_2_3_4_5.add(panel3,"panel3");
+        panel_2_3_4_5.add(panel4, "panel4");
+        panel_2_3_4_5.add(panel5, "panel5");
+
+        f_Index.add(panel_2_3_4_5,BorderLayout.CENTER);
+    }
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+
+
+
+}
---
+++ Garapen inguruneak/5_3_mugarria/testak/src/erleak/Login.java
@@ -0,0 +1,334 @@
+package erleak;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+import java.awt.*;
+import java.awt.event.*;
+import java.io.IOException;
+
+
+public class Login extends JFrame {
+    private JFrame f_login= new JFrame();
+    private JPanel panel1, panel2, panel3, panel4, panel5, panel_1_4, panel_3_5;
+    private CardLayout card1, card2;
+    private JButton erregistratu, login ,urrengoa, itzuli1,itzuli2;
+    private  JTextField txertatu_izen;
+    private JPasswordField txertatupass;
+    private JLabel mezua;
+    private String identifikatzaile;
+
+    public void sortu_login(){
+
+        nothr();
+        center1();
+        south();
+        login_orria();
+    }
+    public void login_orria(){
+        f_login.setTitle("Login");
+        f_login.setSize(400, 250);
+        f_login.setVisible(true);
+        f_login.setLocationRelativeTo(null);
+        f_login.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f_login.setLayout(new BorderLayout());
+
+    }
+
+    public void nothr(){
+        panel2 = new JPanel();
+        panel2.setLayout(new BoxLayout(panel2,BoxLayout.X_AXIS));
+        ImageIcon fondoa_irudia = new ImageIcon(".\\Irudiak\\kuadroa_erdia.jpg");
+        ImageIcon fondo_aldatuta= new ImageIcon(fondoa_irudia.getImage().getScaledInstance(400, 80, java.awt.Image.SCALE_SMOOTH));
+        JLabel irudia = new JLabel(fondo_aldatuta);
+        panel2.add(irudia);
+        f_login.add(panel2, BorderLayout.NORTH);
+    }
+
+
+    public void center1(){
+        card1= new CardLayout();
+        panel_1_4 = new JPanel();
+        panel_1_4.setLayout(card1);
+
+        panel1 = new JPanel(null);
+
+        txertatu_izen = new JTextField(17);
+        txertatu_izen.setEnabled(false);
+        txertatu_izen.addMouseListener(new MouseListener() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                txertatu_izen.setEnabled(true);
+            }
+
+            @Override
+            public void mousePressed(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+
+            }
+        });
+
+        txertatu_izen.setForeground(Color.GRAY);
+        txertatu_izen.setText("Izena, NAN, Telefonoa edo Emaila");
+        txertatu_izen.addFocusListener(new FocusListener() {
+            @Override
+            public void focusGained(FocusEvent e) {
+                SwingUtilities.invokeLater(() -> {
+                    if (txertatu_izen.getText().equals("Izena, NAN, Telefonoa edo Emaila")) {
+                        txertatu_izen.setText(" ");
+                        txertatu_izen.setForeground(Color.BLACK);
+                    }
+                });
+            }
+            @Override
+            public void focusLost(FocusEvent e) {
+                SwingUtilities.invokeLater(() -> {
+                    if (txertatu_izen.getText().equals(" ")) {
+                        txertatu_izen.setText("Izena, NAN, Telefonoa edo Emaila");
+                        txertatu_izen.setForeground(Color.GRAY);
+                    }
+                });
+            }
+        });
+
+        txertatu_izen.getDocument().addDocumentListener(new DocumentListener() {
+            @Override
+            public void insertUpdate(DocumentEvent e) {
+                SwingUtilities.invokeLater(() -> {
+                    if (txertatu_izen.getText().isEmpty()) {
+                        txertatu_izen.setText("Izena, NAN, Telefonoa edo Emaila");
+                        txertatu_izen.setForeground(Color.GRAY);
+                    }
+                });
+            }
+
+            @Override
+            public void removeUpdate(DocumentEvent e) {
+
+            }
+
+            @Override
+            public void changedUpdate(DocumentEvent e) {
+
+            }
+        });
+
+        mezua = new JLabel("");
+
+        txertatu_izen.setBounds(95,10,200, 25);
+        mezua.setBounds(105, 30, 200, 50);
+
+        panel1.add(txertatu_izen);
+        panel1.add(mezua);
+
+        panel4 = new JPanel();
+
+        txertatupass = new JPasswordField(18);
+        txertatupass.setEnabled(false);
+        txertatupass.setEchoChar((char) 0);
+        txertatupass.addMouseListener(new MouseListener() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                txertatupass.setEnabled(true);
+            }
+
+            @Override
+            public void mousePressed(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+
+            }
+        });
+
+
+        txertatupass.setForeground(Color.GRAY);
+        txertatupass.setText("Pasahitza");
+
+        txertatupass.addFocusListener(new FocusListener() {
+            @Override
+            public void focusGained(FocusEvent e) {
+                SwingUtilities.invokeLater(() -> {
+                    String pasahitza= new String(txertatupass.getPassword());
+                    if (pasahitza.equals("Pasahitza")) {
+                        txertatupass.setText("");
+                        txertatupass.setForeground(Color.BLACK);
+                        txertatupass.setEchoChar('*');
+                    }
+                });
+            }
+            @Override
+            public void focusLost(FocusEvent e) {
+                SwingUtilities.invokeLater(() -> {
+                    String pasahitza= new String(txertatupass.getPassword());
+                    if (pasahitza.equals("")) {
+                        txertatupass.setText("Pasahitza");
+                        txertatupass.setForeground(Color.GRAY);
+                        txertatupass.setEchoChar((char) 0);
+                    }
+                });
+            }
+        });
+
+        txertatupass.getDocument().addDocumentListener(new DocumentListener() {
+            @Override
+            public void insertUpdate(DocumentEvent e) {
+                SwingUtilities.invokeLater(() -> {
+                    String pasahitza= new String(txertatupass.getPassword());
+                    if (pasahitza.isEmpty()) {
+                        txertatupass.setText("Pasahitza");
+                        txertatupass.setForeground(Color.GRAY);
+                        txertatupass.setEchoChar((char) 0);
+                    }
+                });
+            }
+
+            @Override
+            public void removeUpdate(DocumentEvent e) {
+
+            }
+
+            @Override
+            public void changedUpdate(DocumentEvent e) {
+
+            }
+        });
+
+        panel4.add(txertatupass);
+
+        panel_1_4.add(panel1, "panel1");
+        panel_1_4.add(panel4, "panel4");
+
+        f_login.add(panel_1_4,  BorderLayout.CENTER);
+    }
+
+
+    public void south(){
+        card2= new CardLayout();
+        panel_3_5= new JPanel();
+        panel_3_5.setLayout(card2);
+
+        panel3 = new JPanel();
+        panel3.setLayout(new BoxLayout(panel3,BoxLayout.X_AXIS));
+
+        JLabel tartea1= new JLabel("                    ");
+        urrengoa = new JButton("Hurrengoa");
+        erregistratu = new JButton("Erregistratu");
+        itzuli1 = new JButton("Atzera");
+
+        panel3.add(tartea1);
+        panel3.add(itzuli1);
+        panel3.add(erregistratu);
+        panel3.add(urrengoa);
+
+        itzuli1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                new Index().sortu();
+                f_login.dispose();
+            }
+        });
+
+        erregistratu.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                new Erregistratu().sortu_Erregistratu();
+            }
+        });
+        urrengoa.addActionListener(new ActionListener() {
+            // Pasahitza jarri al izateko, aldatzen du.
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (!txertatu_izen.getText().isEmpty() && !txertatu_izen.getText().equals("Izena, NAN, Telefonoa edo Emaila")) {
+                    identifikatzaile = txertatu_izen.getText();
+                    card2.show(panel_3_5, "panel5");
+                    card1.show(panel_1_4, "panel4");
+                    mezua.setText("");
+                }else {
+                    mezua.setForeground(Color.red);
+                    mezua.setText("Identifikatzailea sartu behar da");
+                }
+            }
+        });
+
+
+
+        panel5 = new JPanel();
+        panel5.setLayout(new BoxLayout(panel5,BoxLayout.X_AXIS));
+
+        JLabel tartea2= new JLabel("                          ");
+        login = new JButton("Login");
+        erregistratu = new JButton("Erregistratu");
+        itzuli2 = new JButton("Atzera");
+
+        panel5.add(tartea2);
+        panel5.add(itzuli2);
+        panel5.add(erregistratu);
+        panel5.add(login);
+
+        itzuli2.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //Pasahitzatik atzera bueltatzeko
+                card2.show(panel_3_5, "panel3");
+                card1.show(panel_1_4, "panel1");
+                txertatu_izen.setText(identifikatzaile);
+            }
+        });
+
+        erregistratu.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(null, "Erregistroko funtzioa sortu gabe");
+            }
+        });
+        login.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (true){
+
+                }
+            }
+        });
+
+
+        panel_3_5.add(panel3, "panel3");
+        panel_3_5.add(panel5, "panel5");
+        f_login.add(panel_3_5, BorderLayout.SOUTH);
+
+    }
+
+    public static void main(String[] args){
+        new Login().sortu_login();
+    }
+
+
+}
