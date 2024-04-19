2d056e820faf4831873f58b7ae7533f8e740411d
iarrizabaloa <iarrizabaloai23wg@ikzubirimanteo.com>
2024-04-19 10:58:12 +0200

    Lehen probak

---
+++ Garapen inguruneak/5_3_mugarria/testak/src/test/FocusGainedTest.java
@@ -0,0 +1,13 @@
+package test;
+
+import erleak.Login;
+import static org.junit.Assert.*;
+import org.junit.Test;
+public class FocusGainedTest extends Login {
+
+
+    /*@Test
+    public void testFocusGained(){
+        assertTrue(Login().focusGained());
+    }*/
+}
---
+++ Garapen inguruneak/5_3_mugarria/testak/src/test/sortuLoginTest.java
@@ -0,0 +1,19 @@
+package test;
+
+import erleak.Login;
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+
+
+public class sortuLoginTest extends Login {
+
+
+    /*@Test
+    public void sortuLoginNotNull(){
+        assertNotNull("Objetu denak sortuta izan behar dute", new Login().sortu_login());
+    }*/
+
+
+
+}
